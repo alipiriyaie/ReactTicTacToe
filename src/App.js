9416ec7b4297c93a6b7c0d7c7dcab30eb4243746
Ali Piriyaie <piriyaie@googlemail.com>
2023-03-26 17:50:08 +0200

    refactored

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import './App.css';
 
 
-function Square({value, onSquareClick}){
+function Square({value, onSquareClick, index}){
   return <button className="square" onClick={onSquareClick}>{value}</button>
 }
 
@@ -31,24 +31,38 @@ function Board({xIsNext, squares, onPlay}) {
     status = "Next player: " + (xIsNext ? 'X' : 'O');
   }
 
+  const renderSquare = (i) => {
+    return <Square
+              value={squares[i]}
+              onSquareClick={() => handleClick(i)}
+              key={i}
+            />;
+  }
+
+  const renderBoardRow = (start) => {
+    const boardRow = [];
+    for (let i = start; i < start + 3; i++) {
+      boardRow.push(renderSquare(i));
+    }
+    return boardRow;
+  }
+
+  const renderBoard = () => {
+    const board = [];
+    for (let i = 0; i < 9; i += 3) {
+      board.push(
+        <div className="board-row" key={i}>
+          {renderBoardRow(i)}
+        </div>
+      );
+    }
+    return board;
+  }
+
   return (
-    <>
+    <>    
       <div className='status'>{status}</div>
-      <div className='board-row'>
-        <Square value={squares[0]} onSquareClick={() => handleClick(0)}/>  
-        <Square value={squares[1]} onSquareClick={() => handleClick(1)}/>
-        <Square value={squares[2]} onSquareClick={() => handleClick(2)}/>
-      </div>
-      <div className='board-row'>
-        <Square value={squares[3]} onSquareClick={() => handleClick(3)}/>  
-        <Square value={squares[4]} onSquareClick={() => handleClick(4)}/>
-        <Square value={squares[5]} onSquareClick={() => handleClick(5)}/>
-      </div>
-      <div className='board-row'>
-        <Square value={squares[6]} onSquareClick={() => handleClick(6)}/>  
-        <Square value={squares[7]} onSquareClick={() => handleClick(7)}/>
-        <Square value={squares[8]} onSquareClick={() => handleClick(8)}/>
-      </div>
+      {renderBoard()}
     </>
   );
 }
